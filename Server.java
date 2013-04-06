0e3d49248ff8faaa6073428ecdb9e64766aedf07
nicholasxjy <yitaqi2011@gmail.com>
2013-04-06 10:40:21 +0800

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,17 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+  public static void main(String[] args) throws Exception {
+		ServerSocket ss = new ServerSocket(8888);
+		Socket client = null;
+		boolean flag = true;
+		System.out.println("waiting for clients connect...");
+		while (flag) {
+			
+			new Thread(new ClientThread(ss.accept())).start();
+		}
+		ss.close();
+	}
+}
